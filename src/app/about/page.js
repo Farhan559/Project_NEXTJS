397a81f6c85890b71574f45a52bbf5441eb3bab1
Farhan559 <farhansiddiqui559@gmail.com>
2024-11-27 18:57:37 +0500

    feat: Add Routing

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+
+
+const about = () => {
+  return (
+    <>
+      <h2>About Us</h2>
+    </>
+  )
+}
+
+export default about
---
+++ src/app/home/page.js
@@ -0,0 +1,11 @@
+
+
+const home = () => {
+  return (
+    <>
+      <h2>Home</h2>
+    </>
+  )
+}
+
+export default home
