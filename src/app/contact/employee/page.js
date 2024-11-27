707e9f90e29e8f13526c461dc8b4b35a9efcffd1
Farhan559 <farhansiddiqui559@gmail.com>
2024-11-27 20:13:03 +0500

    feat : Add Nested Routing

---
+++ src/app/contact/employee/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+const employee = () => {
+    return (
+      <>
+        <h2>Contact employee</h2>
+        <Link href='/contact'>Go Back</Link>
+      </>
+    )
+  }
+  
+  export default employee
+  
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,5 +1,5 @@
 'use client'
-// import Link from "next/link"
+import Link from "next/link"
 import { useRouter } from "next/navigation"
 
 const contact = () => {
@@ -10,15 +10,23 @@ const contact = () => {
   return (
     <>
       <h2>Contact Us</h2>
+      <Link href='/contact/company'>Company</Link>
+      <br />
+      <Link href='/contact/employee'>Employee</Link>
     {/* <Link href='/about'>about</Link>
     <br></br>
     <Link href='/home'>home</Link> */}
     {/* <button onClick={()=>router.push('/about')}>About</button>
     <br />
     <button onClick={()=>router.push('/home')}>home</button> */}
-    <button onClick={()=>handleNavigation('/home')}>Home</button>
+    {/* <button onClick={()=>handleNavigation('/home')}>Home</button>
     <br />
-    <button onClick={()=>handleNavigation('/about')}>About</button>
+    <button onClick={()=>handleNavigation('/about')}>About</button> */}
+    <button onClick={()=>handleNavigation('/contact/company')}>Company</button>
+    <br />
+    <button onClick={()=>handleNavigation('/contact/employee')}>Employee</button>
+    
+
     </>
   )
 }
