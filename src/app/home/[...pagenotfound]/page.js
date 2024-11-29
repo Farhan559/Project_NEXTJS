3a92df3ae35c692ec80e321875e6b3474ebcdb3e
Farhan559 <farhansiddiqui559@gmail.com>
2024-11-29 20:38:31 +0500

    feat : Add Page Not Found For Global and Specific Routes

---
+++ src/app/home/[...pagenotfound]/page.js
@@ -0,0 +1,12 @@
+
+
+const RouteBasedPageNotFound = () => {
+  return (
+    <>
+    <h1>404 Page | Page not found for home Route</h1>
+      
+    </>
+  )
+}
+
+export default RouteBasedPageNotFound
---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const pageNotFound = () => {
+  return (
+    <>
+      <h1> 404 PAge | Page not ound </h1>
+    </>
+  )
+}
+
+export default pageNotFound;
