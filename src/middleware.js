8c31a3fcb84d760e0c99581be4b6e5c101d9c763
Farhan559 <farhansiddiqui559@gmail.com>
2024-12-03 15:54:50 +0500

    fix: changes resolved

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,11 @@
 import { NextResponse } from "next/server";
+// import { config } from "process";
 const isLoggedIn = false;
 export function middleware(request){
-    if(!isLoggedIn && request.nextUrl.pathname != '/home'){
+    // if(!isLoggedIn && request.nextUrl.pathname != '/home'){
         return NextResponse.redirect(new URL('/home',request.url))
-    }
+    // }
+}
+export const config = {
+    matcher: ["/contact/:path"]
 }
